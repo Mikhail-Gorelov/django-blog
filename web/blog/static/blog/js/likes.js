2297afe6e6f72ed19dbd25c8c9adbc77442f3c91
Горелов Михаил <gorelovmihail0@gmail.com>
2022-02-28 10:15:41 +0300

    added like system

--- web/blog/static/blog/js/likes.js
+++ web/blog/static/blog/js/likes.js
@@ -4,10 +4,6 @@ let ApiEndPoint = "/actions/assessment/"
 let ArticleLikeType = "article"
 let LikeVote = 1
 let DislikeVote = 0
-let flagDislike = 0
-let normalLikeCount = Number($("#articleLikeCount").text())
-let likePlusOne = Number($("#articleLikeCount").text()) + 1
-let likeMinusOne = Number($("#articleLikeCount").text()) - 1
 
 $(function () {
   if ($('#articleLikeCount').attr("data-vote") === "1") {
@@ -34,6 +30,10 @@ function postLike() {
         "object_id": $("#articleLikeCount").attr("data"),
         "vote": LikeVote,
       },
+      success: function (data) {
+        $("#articleLikeCount").text("");
+        $("#articleLikeCount").text(" " + data.likes_count);
+      },
     })
 }
 
@@ -47,6 +47,10 @@ function postDislike() {
         "object_id": $("#articleLikeCount").attr("data"),
         "vote": DislikeVote,
       },
+      success: function (data) {
+        $("#articleDislikeCount").text("");
+        $("#articleDislikeCount").text(" " + data.dislike_count);
+      },
     })
 }
 
@@ -54,31 +58,27 @@ function getLikeIcon(e) {
 
   if ($("#articleDislikeIcon").attr("style") === ActiveStyle) {
     $("#articleDislikeIcon").attr("style", NotActiveStyle);
-    if ($('#articleDislikeCount').attr("data-vote") === "0" && flagDislike === 0) {
-      let dislikeCount = Number($("#articleDislikeCount").text()) - 1
-      $("#articleDislikeCount").text("");
-      $("#articleDislikeCount").text(" " + dislikeCount);
-      flagDislike = 1
-    }
+    let dislikeCount = Number($("#articleDislikeCount").text()) - 1
+    $("#articleDislikeCount").text("");
+    $("#articleDislikeCount").text(" " + dislikeCount);
   }
 
   if ($("#articleLikeIcon").attr("style") === NotActiveStyle) {
     $("#articleLikeIcon").attr("style", ActiveStyle);
     postLike();
-    $("#articleLikeCount").text("");
-    $("#articleLikeCount").text(" " + likePlusOne);
   } else {
     $("#articleLikeIcon").attr("style", NotActiveStyle);
     postLike();
-    $("#articleLikeCount").text("");
-    $("#articleLikeCount").text(" " + likeMinusOne);
   }
 }
 
 function getDislikeIcon(e) {
   if ($("#articleLikeIcon").attr("style") === ActiveStyle) {
     $("#articleLikeIcon").attr("style", NotActiveStyle);
-  }
+    let likeCount = Number($("#articleLikeCount").text()) - 1
+    $("#articleLikeCount").text("");
+    $("#articleLikeCount").text(" " + likeCount);
+    }
 
   if ($("#articleDislikeIcon").attr("style") === NotActiveStyle) {
     $("#articleDislikeIcon").attr("style", ActiveStyle);
