a13bc93050f30551a521485783bd48786b92a1d5
Горелов Михаил <gorelovmihail0@gmail.com>
2022-03-01 10:30:12 +0300

    fixed user list dropdown

--- web/user_profile/static/user_profile/js/user-list.js
+++ web/user_profile/static/user_profile/js/user-list.js
@@ -1,6 +1,36 @@
 $(function () {
   $('.send-message').click(sendMessage);
+  let panels = $('.user-infos');  // unique
+  let panelsButton = $('.dropdown-user');
+  panels.hide();
+
+  //Click dropdown
+  panelsButton.click(function () {
+    //get data-for attribute
+    let dataFor = $(this).attr('data-for');  // unique
+    let idFor = $(dataFor);
+
+    //current button
+    let currentButton = $(this);
+    idFor.slideToggle(400, function () {
+      //Completed slidetoggle
+      if (idFor.is(':visible')) {
+        currentButton.html('<i class="icon-chevron-up text-muted"></i>');
+      } else {
+        currentButton.html('<i class="icon-chevron-down text-muted"></i>');
+      }
+    })
+  });
+
+
+  $('[data-toggle="tooltip"]').tooltip();
+
+  $('button').click(function (e) {
+    e.preventDefault();
+    alert("This is a demo.\n :-)");
+  });
 });
+
 function sendMessage() {
   let user_id = $(this).data("id");
   let href = $('.send-message').data("href");
@@ -8,39 +38,5 @@ function sendMessage() {
 
   let url = href + "?auth=" + jwt + "&user_id=" + user_id;
 
-  window.open(url,"_blank").focus();
+  window.open(url, "_blank").focus();
 }
-$(document).ready(function() {
-    var panels = $('.user-infos');
-    var panelsButton = $('.dropdown-user');
-    panels.hide();
-
-    //Click dropdown
-    panelsButton.click(function() {
-        //get data-for attribute
-        var dataFor = $(this).attr('data-for');
-        var idFor = $(dataFor);
-
-        //current button
-        var currentButton = $(this);
-        idFor.slideToggle(400, function() {
-            //Completed slidetoggle
-            if(idFor.is(':visible'))
-            {
-                currentButton.html('<i class="icon-chevron-up text-muted"></i>');
-            }
-            else
-            {
-                currentButton.html('<i class="icon-chevron-down text-muted"></i>');
-            }
-        })
-    });
-
-
-    $('[data-toggle="tooltip"]').tooltip();
-
-    $('button').click(function(e) {
-        e.preventDefault();
-        alert("This is a demo.\n :-)");
-    });
-});
