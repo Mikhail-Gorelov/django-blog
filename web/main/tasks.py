9393add38d53340e486f743fe46cec3778609dc9
Mikhail-Gorelov <58218933+Mikhail-Gorelov@users.noreply.github.com>
2024-04-07 20:18:00 +0200

    feat: created rule to clean up data

--- web/main/tasks.py
+++ web/main/tasks.py
@@ -1,4 +1,7 @@
+from datetime import datetime, timedelta
+
 from src.celery import app
+from drf_api_logger.models import APILogsModel
 
 
 @app.task(name="main.tasks.add", queue="blog")
@@ -14,3 +17,20 @@ def mul(x, y):
 @app.task(name="main.tasks.xsum", queue="blog")
 def xsum(numbers):
     return sum(numbers)
+
+
+@app.task(name="main.tasks.cleanup", queue="blog")
+def cleanup():
+    # Get the current date
+    current_date = datetime.now()
+
+    # Calculate the first day of the current month
+    first_day_of_current_month = current_date.replace(day=1)
+
+    # Calculate the last day of the previous month
+    last_day_of_previous_month = first_day_of_current_month - timedelta(days=1)
+
+    # Calculate the first day of the previous month
+    first_day_of_previous_month = last_day_of_previous_month.replace(day=1)
+    APILogsModel.objects.filter(added_on__gte=first_day_of_previous_month, added_on__lte=last_day_of_previous_month).delete()
+    return
